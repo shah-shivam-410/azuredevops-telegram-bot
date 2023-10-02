d49b750d0e8c79586626a0c6f10cbfae085c6c61
shah-shivam-410 <shah.shivam.410@gmail.com>
2023-10-02 17:56:59 +0530

    Added ADO operations

--- src/main/java/ado/modal/WorkItem.java
+++ src/main/java/ado/modal/WorkItem.java
@@ -5,58 +5,108 @@ public class WorkItem {
 	Integer id;
 	String title;
 	String creationDate;
+	String areaPath;
 	String type;
+	String status;
 	String createdBy;
 	Integer priority;
+	Integer noOfTCsBlocked;
+	String assignedToTeam;
+	
+	@Override
+	public String toString() {
+		return "<b>WorkItem</b>\n"
+			+ "<b>id : </b>" + id + "\n"
+			+ "<b>title : </b>" + title + "\n"
+			+ "<b>creationDate : </b>" + creationDate + "\n"
+			+ "<b>areaPath : </b>" + areaPath + "\n"
+			+ "<b>type : </b>" + type + "\n"
+			+ "<b>status : </b>" + status + "\n"
+			+ "<b>createdBy : </b>" + createdBy + "\n"
+			+ "<b>priority : </b>" + priority + "\n"
+			+ "<b>noOfTCsBlocked : </b>" + noOfTCsBlocked + "\n"
+			+ "<b>assignedToTeam : </b>" + assignedToTeam + "\n"
+			;
+	}
 	
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getCreationDate() {
 		return creationDate;
 	}
+
 	public void setCreationDate(String creationDate) {
 		this.creationDate = creationDate;
 	}
+
+	public String getAreaPath() {
+		return areaPath;
+	}
+
+	public void setAreaPath(String areaPath) {
+		this.areaPath = areaPath;
+	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	public String getCreatedBy() {
 		return createdBy;
 	}
+
 	public void setCreatedBy(String createdBy) {
 		this.createdBy = createdBy;
 	}
+
 	public Integer getPriority() {
 		return priority;
 	}
+
 	public void setPriority(Integer priority) {
 		this.priority = priority;
 	}
-	@Override
-	public String toString() {
-		return "<b>WorkItem</b>\n"
-				+ "<b>id : </b>" + id + "\n"
-				+ "<b>title : </b>" + title + "\n"
-				+ "<b>creationDate : </b>" + creationDate + "\n"
-				+ "<b>type : </b>" + type + "\n"
-				+ "<b>createdBy : </b>" + createdBy + "\n"
-				+ "<b>priority : </b>" + priority + "\n"
-				;
+
+	public Integer getNoOfTCsBlocked() {
+		return noOfTCsBlocked;
 	}
-	
-	
+
+	public void setNoOfTCsBlocked(Integer noOfTCsBlocked) {
+		this.noOfTCsBlocked = noOfTCsBlocked;
+	}
+
+	public String getAssignedToTeam() {
+		return assignedToTeam;
+	}
+
+	public void setAssignedToTeam(String assignedToTeam) {
+		this.assignedToTeam = assignedToTeam;
+	}
 
 }
--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -1,7 +1,5 @@
 package bot;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
